5bf4164f3f44d03d33d8ed899edd105d6677ed58
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-08-21 11:51:50 +0100

    Use exception-based API backoff

--- evals/utils/api_utils.py
+++ evals/utils/api_utils.py
@@ -7,16 +7,13 @@ EVALS_THREAD_TIMEOUT = float(os.environ.get("EVALS_THREAD_TIMEOUT", "40"))
 logging.getLogger("httpx").setLevel(logging.WARNING)  # suppress "OK" logs from openai API calls
 
 
-@backoff.on_predicate(
-    wait_gen=backoff.expo,
-    max_value=60,
-    factor=1.5,
-)
 def create_retrying(func: callable, retry_exceptions: tuple[Exception], *args, **kwargs):
-    """
-    Retries given function if one of given exceptions is raised
-    """
-    try:
-        return func(*args, **kwargs)
-    except retry_exceptions:
-        return False
+    """Call ``func`` and retry only the configured exceptions."""
+    retrying_func = backoff.on_exception(
+        wait_gen=backoff.expo,
+        exception=retry_exceptions,
+        max_value=60,
+        factor=1.5,
+        max_time=EVALS_THREAD_TIMEOUT,
+    )(func)
+    return retrying_func(*args, **kwargs)
